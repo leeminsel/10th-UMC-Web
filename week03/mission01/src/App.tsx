5ab75e1a4859ae947eee3c7cb6026674e8e1a150
LEEMINSEO <mi1789837@gmail.com>
2026-04-01 15:36:31 +0900

    feat:영화 상세페이지 구현

--- week03/mission01/src/App.tsx
+++ week03/mission01/src/App.tsx
@@ -1,14 +1,47 @@
 import './App.css'
+import HomePage from './pages/HomePage';
 import MoviePage from './pages/MoviePage';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NotFoundPage from './pages/NotFoundPage';
+import  MovieDetailPage  from './pages/MovieDetailPage';
+
+// createBrowserRouter v6 기준
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <HomePage />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        path:'movies/:category',
+        element: <MoviePage/>,
+      },
+     {
+      path:'movie/:movieId',
+      element: <MovieDetailPage/>,
+     }
+    ]
+  },
+]);
+
+// movies/upcoming
+// movies/popular
+// movies/now_playing
+// movies/top_rated
+// movies/category{movie.id}
+
+// 쿼리파라미터 추천하심
+// movies?category=upcoming
+// movies?category=popular
+// movie/123
+
+
+
 
 function App() {
-  console.log(import.meta.env.VITE_TMDB_KEY);
 
-  return (
-  <>
-    <MoviePage />
-  </>
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App
