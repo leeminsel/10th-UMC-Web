259a7431b2662a20adec47f961a39e9760dabdb0
LEEMINSEO <mi1789837@gmail.com>
2026-04-08 16:30:40 +0900

    feat:Custom Hook으로 영화 데이터 불러오기

---
+++ week04/mission01/src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import { Navbar } from "../components/Navbar";
+// <Outlet/> -> 무슨역할?
+
+const HomePage=() => {
+    return <div>
+        
+        <Navbar/>
+        <Outlet/>
+    </div>
+};
+
+export default HomePage;
