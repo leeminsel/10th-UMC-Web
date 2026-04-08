1499ea9bce9b4e6c8f3686d78dfaeb1b01163ebb
LEEMINSEO <mi1789837@gmail.com>
2026-04-08 19:42:35 +0900

    feat:[미션2]로그인 기능 구현

---
+++ week04/mission02/src/layouts/HomeLayout.tsx
@@ -0,0 +1,23 @@
+import { Outlet } from "react-router-dom"
+
+export const HomeLayout = () => {
+  return (
+    <div className="h-dvh flex flex-col">
+        <nav className="flex items-center justify-between px-8 py-4  shadow-lg border-b border-gray-700">
+            <div className="text-xl font-bold text-[#dda5e3]">
+        Minsel
+    </div>
+            <div className="flex gap-3">
+            <button className="px-5 py-2 text-sm font-medium bg-[#dda5e3] text-[#262626] rounded-md hover:bg-[#b2dab1] transition-all duration-200 shadow-md active:scale-95 cursor-pointer">
+                로그인</button>
+            <button className="px-5 py-2 text-sm font-medium bg-[#dda5e3] text-[#262626] rounded-md hover:bg-[#b2dab1] transition-all duration-200 shadow-md active:scale-95 cursor-pointer">
+                회원가입</button>
+            </div>  
+        </nav>
+        <main className="flex-1">
+            <Outlet />
+        </main>
+        <footer className="text-sm text-gray-500">민셀 로그인 화면</footer>
+    </div>
+  )
+}
