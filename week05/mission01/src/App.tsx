1e4a919edbe69ec3f6e6f4106297b7b3dd81453d
LEEMINSEO <mi1789837@gmail.com>
2026-04-29 21:07:13 +0900

    feat:미션3 구글로그인

--- week05/mission01/src/App.tsx
+++ week05/mission01/src/App.tsx
@@ -8,6 +8,7 @@ import { SignupPage } from './pages/SignupPage';
 import { MyPage } from './pages/MyPage';
 import { AuthProvider } from './context/AuthContext';
 import { ProtectedLayout } from './layouts/ProtectedLayout';
+import { GoogleLoginRedirectPage } from './pages/GoogleLoginRedirectPage';
 
 // 1. 홈페이지
 // 2. 로그인 페이지
@@ -23,6 +24,7 @@ const publicRoutes:RouteObject[]=[
     {index: true, element: <HomePage/>},
     {path:"login", element:<LoginPage />},
     {path:"signup", element: <SignupPage/>},
+    {path:"v1/auth/google/callback", element:<GoogleLoginRedirectPage/>},
     ],
   }
 ];
--- week05/mission01/src/pages/LoginPage.tsx
+++ week05/mission01/src/pages/LoginPage.tsx
@@ -26,6 +26,10 @@ export const LoginPage = () => {
         await login(values);
     }
 
+    const handleGoogleLogin=()=> {
+        window.location.href=import.meta.env.VITE_SERVER_API_URL +"/v1/auth/google/login";
+    }
+
     // 오류가 하나라도 있거나, 입력값이 비어있으면 버튼을 비활성화
     const isDisabled= Object.values(errors || {}).some((error:string)=> error.length>0) || // 오류가 있으면 ture
     Object.values(values).some((value:string) => value==="");  // 입력값이 없으면 true
@@ -61,6 +65,14 @@ export const LoginPage = () => {
         disabled={isDisabled} 
         className="w-full bg-[#dda5e3] text-white py-3 rounded-md text-lg font-medium hover:bg-[#b2dab1] transition-colors cursor-pointer disabled:bg-gray-300">
             로그인
+        </button>
+        <button type='button' 
+        onClick={handleGoogleLogin} 
+        className="w-full bg-blue-700 text-white py-3 rounded-md text-lg font-medium hover:bg-blue-500 transition-colors cursor-pointer ">
+            <div className="flex items-center justify-center gap-4">
+                <img src={"image/google.svg"} alt="Google Logo Image" />
+                <span>구글 로그인</span>
+            </div>
         </button>
         </div>
     </div>
