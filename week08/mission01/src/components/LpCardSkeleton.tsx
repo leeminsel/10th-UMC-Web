3314b293327d9cdd1bee4a8db286dc91cd43ad08
이민서 <msl1789837@gmail.com>
2026-05-20 21:50:43 +0900

    Revert "Week08/mission02"

--- week08/mission01/src/components/LpCardSkeleton.tsx
+++
@@ -1,7 +0,0 @@
-const LpCardSkeleton = () => (
-  <div className="rounded-lg overflow-hidden animate-pulse">
-    <div className="w-full aspect-square bg-gray-200" />
-  </div>
-);
-
-export default LpCardSkeleton;
--- week08/mission01/src/constants/key.ts
+++
@@ -1,6 +0,0 @@
-export const LOCAL_STORAGE_KEY = {
-    accessToken: "accessToken",
-    refreshToken: "refreshToken",
-    name: "name",
-    userId: "userId",
-}
