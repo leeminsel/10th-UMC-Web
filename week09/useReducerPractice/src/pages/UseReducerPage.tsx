fa1fcb963f0aac890f794d111bfd4bdbe864822b
LEEMINSEO <mi1789837@gmail.com>
2026-05-27 16:05:25 +0900

    feat:[실습1] useReducer 추가

--- week09/useReducerPractice/src/pages/UseReducerPage.tsx
+++ week09/useReducerPractice/src/pages/UseReducerPage.tsx
@@ -1,15 +1,72 @@
-import { useState } from "react"
+import { useReducer, useState } from "react"
 
+// 1. state 에 대한 interface
+interface IState {
+  counter:number;
+  error:null;
+}
+
+// 2. reducer 에 대한 interface
+interface IAction {
+  type: 'INCREASE' | 'DECREASE' |'RESET_TO_ZERO';
+}
+
+function reducer(state: IState, action: IAction) {
+  const {type}=action; // 구조분해할당
+
+  switch(type) {
+    case 'INCREASE': 
+    return {
+      ... state,
+      counter:state.counter +1,
+    }
+    case 'DECREASE' :
+      return {
+        ... state,
+      counter:state.counter -1,
+      }
+      case 'RESET_TO_ZERO' :
+        return {
+          ... state,
+      counter:0,
+        }
+    default:
+      return state;
+  }
+}
 export default function UseReducerPage() {
+  // 1. useState
   const [count, setCount] = useState(0);
 
-  const handleIncrease=() => {
-    setCount(count+1);
+  // 2. useReducer
+  const [state,dispatch] = useReducer(reducer, {
+    counter:0,
+    error:null,
+  } )
+
+  const handleIncrease = () => {
+    setCount(count + 1);
   }
+
+  console.log(state);
+
   return (
-  <div>
-    <h1>{count}</h1>
-    <button onClick={handleIncrease}>Increase</button>
-  </div>
+    <>
+      <div className="flex flex-col gap-10">
+        <div>
+          <h2 className="text-3xl">useState 훅 사용</h2>
+          <h2>{count}</h2>
+          <button onClick={handleIncrease}>Increase</button>
+        </div>
+        <div>
+          <h2 className="text-3xl">useReducer 훅 사용</h2>
+          <h2>{state.counter}</h2>
+          <button onClick={() => dispatch({type: "INCREASE"})}>Increase</button>
+          <button onClick={() => dispatch({type: "DECREASE"})}>Decrease</button>
+          <button onClick={() => dispatch({type: "RESET_TO_ZERO"})}>Reset</button>
+        </div>
+      </div>
+
+    </>
   )
 }
